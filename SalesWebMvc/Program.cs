864d5897ed1a586debd51495600be5de0c60db4c
ResleyDietrich <resleydietrich@gmail.com>
2024-03-19 21:35:05 -0300

    Seeding Service

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using SalesWebMvc.Data;
@@ -11,21 +12,34 @@ var connectionStr = builder.Configuration.GetConnectionString("SalesWebMvcContex
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 options.UseMySql(connectionStr, ServerVersion.AutoDetect(connectionStr), builder => builder.MigrationsAssembly("SalesWebMvc")));
 
+builder.Services.AddScoped<SeedingService>();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+
+    var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<SalesWebMvcContext>();
+
+    context.GetService<SeedingService>().Seed();
+
+ }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 
 app.UseRouting();
 
