35c9cd45690b61aac48f990659a19aa049a1b98f
ResleyDietrich <resleydietrich@gmail.com>
2024-03-27 21:54:18 -0300

    Implementing simple search

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -20,6 +20,7 @@ options.UseMySql(connectionStr, ServerVersion.AutoDetect(connectionStr), builder
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<SalesRecordService>();
 
 var ptBR = new CultureInfo("pt-BR");
 var localizationOptions = new RequestLocalizationOptions
